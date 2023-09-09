ddf798e6bfaf651cdeedde703770535e24507827
Vinicius Carmo <131480329+Running-Agama@users.noreply.github.com>
2023-09-09 00:51:38 -0300

    Add files via upload

---
+++ separate projects/Simple List/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+
+string? resposta;
+
+var nomes = new List<String> {"coisalegal1", "coisalegal2"};
+
+do
+{
+
+    Console.WriteLine("Deseja adicionar um novo item na lista? (y ou n)");
+    resposta = Console.ReadLine();
+
+    if(resposta?.ToLower().Trim() == "y")
+    {
+        Console.WriteLine("Digite o nome do item");
+        nomes.Add(Console.ReadLine());
+    }
+
+}
+while(resposta.ToLower().Trim() == "y");
+
+
+Console.Clear();
+
+foreach(string nome in nomes)
+    Console.WriteLine(nome);
+
+Console.WriteLine($"Atualmente temos {nomes.Count} itens na lista");
+//no futuro ver como salvar os itens em um arquivo de texto modificavel
+
