d5af328081103c79828c968841989826656ef657
Vinicius Carmo <131480329+Running-Agama@users.noreply.github.com>
2023-09-09 00:42:53 -0300

    Organizing the repository

---
+++ separate projects/simple speed calculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program {
+  public static void Main (string[] args) 
+  {
+    
+    int speedObjectOne = 0, speedObjectTwo = 0, weightObjectOne = 0, weightObjectTwo = 0;
+    int factoringOne, weightSum, factoringTwo, factoringSum, finalSpeed;
+    
+
+    Console.Write($"Enter Object One's weight: ");
+    weightObjectOne = int.Parse(Console.ReadLine());
+    
+    Console.Write("Enter Object Two's weight: ");
+    weightObjectTwo = int.Parse(Console.ReadLine());
+    
+    Console.Write("Enter Object One's speed: ");
+    speedObjectOne = int.Parse(Console.ReadLine());
+
+    Console.Write("Enter Object Two's speed: ");
+    speedObjectTwo = int.Parse(Console.ReadLine());
+    
+    factoringOne = weightObjectOne * speedObjectOne;
+    factoringTwo = weightObjectTwo * speedObjectTwo;
+    weightSum = weightObjectOne + weightObjectTwo;
+    factoringSum = factoringOne + factoringTwo;
+    finalSpeed = factoringSum / weightSum;
+
+    Console.WriteLine($"Final speed is {finalSpeed}");  
+  }
+}
